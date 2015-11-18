6d8406c9c1bc901955bf919080e3aa25fbfbc3a8
Mike Willard <mwillard@izotope.com>
2015-11-17 23:02:43 -0500

    Adding youtube_extension

---
+++ youtube_extension.js
@@ -0,0 +1,23 @@
+/* Youtube video loader for Scratch */
+/* Michael Willard, Nov 2015 */
+
+new (function() {
+    var ext = this;
+    
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+    
+    ext.load_video = function(location) {
+        window.open(location, '_blank');
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['', 'load youtube video', 'load_video', 'url'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Youtube extension', descriptor, ext);
+})();
