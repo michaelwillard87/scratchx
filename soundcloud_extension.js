af2fd573114e13834d4ae9f23adfc6963b79f97e
Mike Willard <mwillard@izotope.com>
2015-11-18 00:13:27 -0500

    soundcloud_extension.js

--- soundcloud_extension.js
+++ soundcloud_extension.js
@@ -3,11 +3,13 @@
 	$.getScript("https://connect.soundcloud.com/sdk/sdk-3.0.0.js", function(){
 	   alert("Script loaded but not necessarily executed.");
 	});
-      
-    SC.initialize({
-  		client_id: 'f703c7a6871d553a7db785800792ccb8',
-  		redirect_uri: ' '
-	});
+    
+    ext.init_sc = function() {
+    	SC.initialize({
+  			client_id: 'f703c7a6871d553a7db785800792ccb8',
+  			redirect_uri: ' '
+		});
+    };
 
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
@@ -26,7 +28,8 @@
     var descriptor = {
         blocks: [
             // Block type, block name, function name, default value
-            [' ', 'Play %s', 'play_video', 'YqeW9_5kURI']
+            [' ', 'Play %s', 'play_video', 'YqeW9_5kURI'],
+            [' ', 'Initialize SoundCloud API']
         ]
     };
 
